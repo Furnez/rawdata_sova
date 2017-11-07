ff642d5023abd9faede37aa40305b5476c343184
ZIGNATION\Søren <soerenzig@gmail.com>
2017-11-07 16:27:21 +0100

    Post controller gets

--- API/Controllers/MarkedController.cs
+++ API/Controllers/MarkedController.cs
@@ -2,44 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
     [Route("api/[controller]")]
-    public class PostsController : Controller
+    public class MarkedController : Controller
     {
-        private SOVAContext db = new SOVAContext();
-
-        // GET api/values
+        // GET: api/marked
         [HttpGet]
-        public List<PostsIndhold> Get()
+        public IEnumerable<string> Get()
         {
-            var posts = this.db.postsindhold.ToList<PostsIndhold>();
-
-            return posts;
+            return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
+        // GET: api/Marked/5
+        [HttpGet("{id}", Name = "Get")]
         public string Get(int id)
         {
             return "value";
         }
-
-        // POST api/values
+        
+        // POST: api/Marked
         [HttpPost]
         public void Post([FromBody]string value)
         {
         }
-
-        // PUT api/values/5
+        
+        // PUT: api/Marked/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
         {
         }
-
-        // DELETE api/values/5
+        
+        // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
