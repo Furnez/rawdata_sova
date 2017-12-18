84b2c792ef974e2a2efe91a8ea1691ef89341eb1
DBLJ <j.pecontal@rt-iut.re>
2017-12-18 20:49:37 +0100

    changed behaviour of loading, now look clean

--- API/wwwroot/js/components/searchresult/searchresult.js
+++ API/wwwroot/js/components/searchresult/searchresult.js
@@ -3,13 +3,20 @@
         var posts_search = ko.observableArray([]);
         var showPost = (id) => {console.log(id,params);params.showPost(id);}
         var getPosts = (search) => {
+        //need to call if button clicked again
+                $('.toHideAfterBinding').fadeIn();
+                console.log("salut");
+                    $('.toShowAfterBinding').hide();
             dataservice.getResult(search, (data, err) => {
                 if (err != "error") {
                     posts_search(data);
                     console.log(posts_search()[0].title);
                     console.log(posts_search()[1].title);
                     $('.toHideAfterBinding').fadeOut(1000);
-                    $('.toShowAfterBinding').show();
+                    $('.toShowAfterBinding').show(400);
+                }else{
+                    $('.toHideAfterBinding').fadeOut(1000);
+                    $('.toShowAfterBinding').show(400);
                 }
             });
         };
